7aafc10421c94904e109ce0d84ad80ec54acf6e8
alex <331349470@qq.com>
2020-12-11 12:10:07 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/','StaticPagesController@home');
+route::get('/help','StaticPagesController@help');
+route::get('/about','StaticPagesController@about');
+
+
